7280358fe4040847ef407997c9551407ecb27a26
André Moreira <arm332@protonmail.com>
2022-08-09 09:23:29 -0300

    Passa tudo para HTML estático e atualiza perfis profissionais

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('SMTP_ADDR', 'secretariat@matthieudeboisseson.com');
-define('SMTP_NAME', 'Boisseson Arbitration');
-define('SMTP_HOST', '');
-define('SMTP_USER', '');
-define('SMTP_PASS', '');
-?>
